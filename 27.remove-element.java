a700ada718c092e040e25c68f0e400f092490522
M Abdullah <abdullahmohammad7972@gmail.com>
2025-05-20 13:55:41 +0530

    Arrays Problems

---
+++ 27.remove-element.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=26 lang=java
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+class Solution {
+    public int removeDuplicates(int[] nums, int val) {
+        int j=0;
+        for(int i=0; i<nums.length; i++){
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        } return j;
+    }
+}
+// @lc code=end
+
---
+++ 35.search-insert-position.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int j=0;
+        return j;
+    }
+}
+// @lc code=end
+
