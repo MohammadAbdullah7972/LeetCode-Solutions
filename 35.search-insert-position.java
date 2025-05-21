a4800eb531712abf6cb7a360443f23d9a893cb75
M Abdullah <abdullahmohammad7972@gmail.com>
2025-05-21 10:51:51 +0530

    problem no 35

--- 35.search-insert-position.java
+++ 35.search-insert-position.java
@@ -3,12 +3,24 @@
  *
  * [35] Search Insert Position
  */
-
+// [1,3,5,6]
 // @lc code=start
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int j=0;
-        return j;
+        int left = 0;
+        int right = nums.length;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid-1;
+            } else{
+                left = mid+1;
+            }
+        } return left;
     }
 }
 // @lc code=end
