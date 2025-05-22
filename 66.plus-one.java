41ee8d7b8e9b27ece8d679520724f919d29b2245
M Abdullah <abdullahmohammad7972@gmail.com>
2025-05-22 11:05:43 +0530

    question 66 plus-one

--- 66.plus-one.java
+++ 66.plus-one.java
@@ -7,7 +7,19 @@
 // @lc code=start
 class Solution {
     public int[] plusOne(int[] digits) {
-        
+        int n = digits.length;
+        int numbers[] = new int[digits.length + 1];
+
+        for(int i= n-1; i>=0; i--){
+            if (digits[i] == 9) {
+                digits[i] =0;
+            } else {
+                digits[i]++;
+                return digits;
+            }
+        }
+        numbers[0] = 1;
+        return numbers;
     }
 }
 // @lc code=end
