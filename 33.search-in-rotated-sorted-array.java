f633261cc0f63a25a8bbee64804ae57c8ea8074a
M Abdullah <abdullahmohammad7972@gmail.com>
2025-05-24 13:26:31 +0530

    question 33 medium search element in routated array modified binary-search algo

--- 33.search-in-rotated-sorted-array.java
+++ 33.search-in-rotated-sorted-array.java
@@ -4,12 +4,40 @@
  * [33] Search in Rotated Sorted Array
  * [Array/ Medium]
  */
+// Array [4,5,6,7, 0,1,2]
 
 // @lc code=start
 class Solution {
     public int search(int[] nums, int target) {
-        return 1;
+        if(nums.length==1 && nums[0]==target){
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left <= right) {
+            int mid = (left + right) /2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (target > nums[mid] || target < nums[left] ) {
+                    left = mid +1;
+                } else {
+                    right = mid-1;
+                }
+            } else {
+                if (target < nums[mid] || target > nums[right]) {
+                    right = mid-1;
+                } else {
+                    left = mid +1;
+                }
+            }
+        }
+        return -1;
     }
 }
 // @lc code=end
 
+// Array [4,5,6,7, 0,1,2]
