2d6409825ccd94a539b1ed119ae1dd51e02a7670
M Abdullah <abdullahmohammad7972@gmail.com>
2025-05-28 18:53:28 +0530

    question 80 solved using two pointers slow,fast

--- 80.remove-duplicates-from-sorted-array-ii.java
+++ 80.remove-duplicates-from-sorted-array-ii.java
@@ -7,7 +7,16 @@
 // @lc code=start
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
+        int slow = 2;
+        for(int fast=2; fast<nums.length; fast++){
+            if(nums[slow-2] == nums[fast]){
+                continue;
+            } else {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
     }
 }
 // @lc code=end
