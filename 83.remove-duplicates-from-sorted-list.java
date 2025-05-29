1d7d164db84b98f7119323ebcb5f40dfd0ad6fc6
M Abdullah <abdullahmohammad7972@gmail.com>
2025-05-29 18:52:59 +0530

    remove duplicates from the list using recursivly, iterative[single pointer technique]

--- 83.remove-duplicates-from-sorted-list.java
+++ 83.remove-duplicates-from-sorted-list.java
@@ -5,23 +5,38 @@
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+import java.lang.classfile.components.ClassPrinter.ListNode;
+//   Definition for singly-linked list.
+// public class ListNode {
+//       int val;
+//       ListNode next;
+//       ListNode() {}
+//       ListNode(int val) { this.val = val; }
+//       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//   }
+
 // [Linked List/ Easy]
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        
-        ListNode current;
-        return current;
+        if (head == null || head.next == null)
+            return head;
+
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+
+        // ListNode cur = head;
+        // if (head == null || head.next == null) return head;
+
+        // while (cur != null && cur.next != null) {
+        // if (cur.val == cur.next.val) {
+        // cur.next = cur.next.next;
+        // } else{
+        // cur = cur.next;
+        // }
+        // }
+        // return head;
+
     }
 }
 // @lc code=end
-
