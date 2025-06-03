b876e560cdd7ed457780aa5cfb49b8ada091b130
M Abdullah <abdullahmohammad7972@gmail.com>
2025-06-03 21:37:38 +0530

    find single number using bit manimulation, XOR

--- 136.single-number.java
+++ 136.single-number.java
@@ -7,7 +7,11 @@
 // @lc code=start
 class Solution {
     public int singleNumber(int[] nums) {
-        
+        int result = 0;
+        for(int num: nums){
+            result = result ^ num;
+        }
+        return result;
     }
 }
 // @lc code=end
