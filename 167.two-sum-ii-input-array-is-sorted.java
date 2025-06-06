5ee7e26d38f5c9ba31387ba8d61af70f193306b7
M Abdullah <abdullahmohammad7972@gmail.com>
2025-06-06 18:49:06 +0530

    using two pointer approach

--- 167.two-sum-ii-input-array-is-sorted.java
+++ 167.two-sum-ii-input-array-is-sorted.java
@@ -7,7 +7,20 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        
+        int left = 0;
+        int right = numbers.length-1;
+
+        while (left < right) {
+            int cal = numbers[left] + numbers[right]; 
+            if (target == cal) {
+               return new int[]{left, right}; 
+            } else if (cal > target) {
+                right--;
+            } else{
+                left++;
+            }
+        }
+        throw new IllegalArgumentException("No any Input");
     }
 }
 // @lc code=end
